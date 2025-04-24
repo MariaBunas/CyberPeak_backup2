17471c56eaa16c4d811c46985b2ed4338497bf28
MariaBunas <maria.bunas@e-cdloga.ro>
2025-04-24 12:11:52 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const map = L.map('map').setView([45.9432, 24.9668], 6);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+let markers = [];
+map.on('click', function (e) {
+    const marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
+    markers.push(marker);
+});
+
+function clearMarkers() {
+    markers.forEach(marker => map.removeLayer(marker));
+    markers = [];
+}
